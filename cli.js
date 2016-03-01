6469ebd44f3ef8f226a508f0f0085e157103cdab
Karissa McKelvey <krmckelv@gmail.com>
2016-02-29 16:16:24 -0800

    update paths by removing app/

---
+++ cli.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+var args = require('minimist')(process.argv.slice(2))
+var server = require('./server.js')
+
+var port = 8080 || process.env.PORT
+
+server.listen(port, function () {
+  console.log('http://localhost:' + port)
+})
