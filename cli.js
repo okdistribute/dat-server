c72ed5f3f9b9db08d853d1739552968ebd1fc469
Karissa McKelvey <krmckelv@gmail.com>
2016-03-10 16:46:10 -0800

    8080 as fallback port

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 var args = require('minimist')(process.argv.slice(2))
 var server = require('./server.js')
 
-var port = 8080 || process.env.PORT || args.PORT
+var port = process.env.PORT || args.PORT || 8080
 
 server.listen(port, function () {
   console.log('http://localhost:' + port)
