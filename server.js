c5b0c0def203067a0987eb1738c3eea21d47dc26
Max Ogden <max@maxogden.com>
2016-04-12 11:12:41 -0700

    WIP add new methods to server

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-var Dat = require('./')
+var Dat = require('./dat.js')
 var from = require('from2')
 
 module.exports = function (server, stream) {
   var dat = Dat()
   return {
-    join: function (link, dir) {
-      var progress = dat.join(link, dir)
-      var progressStream = from.obj()
-      progress.on('stats', function () {
-        progressStream.write(progress.stats)
-      })
-      return progressStream
+    status: function (cb) {
+      // returns {
+      //   '23i4jo2i34jo233i4j': {
+      //     connected: 5,
+      //     uploadRate: 23084,
+      //   }
+      // }
     },
-    leave: function (id) {
+    link: function (cb) {
+      // return stream of import stats
+
+    },
+    join: function (link, dir, cb) {
+      var progress = dat.join(link, dir)
+    },
+    leave: function (id, cb) {
       dat.leave(id)
     },
     close: function (cb) {
