f55b583baf4e751b6761979315aa53f8d28dabc9
karissa <krmckelv@gmail.com>
2016-04-13 16:40:14 -0700

    first pass at high level js api

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var client = require('./client.js')
 
 module.exports = Dat
 
-function Dat () {
-
-}
+function Dat () { }
 
 Dat.prototype.join = function (link, dir, cb) {
   client(function (err, rpc, conn) {
@@ -18,16 +16,42 @@ Dat.prototype.join = function (link, dir, cb) {
 }
 
 Dat.prototype.link = function (dir, cb) {
-
+  client(function (err, rpc, conn) {
+    if (err) return cb(err)
+    rpc.link(dir, function (err, link) {
+      if (err) return cb(err)
+      conn.destroy()
+      cb(null, link)
+    })
+  })
 }
 
 Dat.prototype.status = function (cb) {
+  client(function (err, rpc, conn) {
+    if (err) return cb(err)
+    rpc.status(function (err, status) {
+      if (err) return cb(err)
+      conn.destroy()
+      cb(null, status)
+    })
+  })
 }
 
 Dat.prototype.leave = function (link) {
-
+  client(function (err, rpc, conn) {
+    if (err) throw err
+    rpc.leave(link)
+    conn.destroy()
+  })
 }
 
-Dat.prototype.close = function () {
-
+Dat.prototype.close = function (cb) {
+  client(function (err, rpc, conn) {
+    if (err) return cb(err)
+    rpc.close(function (err, status) {
+      if (err) return cb(err)
+      conn.destroy()
+      cb()
+    })
+  })
 }
