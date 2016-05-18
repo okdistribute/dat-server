815ab1c6d73b7e1e0594f22a13948a01463c51a5
karissa <krmckelv@gmail.com>
2016-05-18 11:01:01 -0700

    in dat js test use two separate dats

--- test/dat-js.js
+++ test/dat-js.js
@@ -18,19 +18,21 @@ test('link generates itself', function (t) {
 
 test('replicates link properly', function (t) {
   var dat = Dat()
+  var dat2 = Dat()
   dat.link(FIXTURES_DIR, function (err, link) {
     t.ifError(err)
     t.equals(link.length, 64)
     dat.join(link, FIXTURES_DIR, function (err) {
       t.ifError(err)
-      dat.join(link, DOWNLOAD_DIR, function (err) {
+      dat2.join(link, DOWNLOAD_DIR, function (err) {
         t.ifError(err)
         var files = fs.readdirSync(FIXTURES_DIR)
         files.forEach(function (file) {
-          var downloadPath = path.join('downloads', file)
+          var downloadPath = path.join(DOWNLOAD_DIR, file)
           t.true(fs.existsSync(downloadPath), 'file ' + downloadPath + ' exists')
         })
         dat.close()
+        dat2.close()
         t.end()
       })
     })
